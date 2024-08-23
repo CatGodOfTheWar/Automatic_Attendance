46636e88021661bacc9334302a778e6afe8fc91b
catgodofthewar <chirieacg347@gmail.com>
2024-08-23 22:38:43 +0300

    V.0.3 Admin_Panel Login

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from photo_management import PhotoManagement
 from recognition import Recognition
 from db_management import DBManagement
 
+import customtkinter
+import tkinter.font as tkFont
+
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("green")
 
 class Student:
 
@@ -58,52 +63,54 @@ class Student:
                     self.dBManagement.db_add_student(student)
                 else:
                     print(f"Student {student} already registered")
-        
-def main():
 
-    username = input("Enter student name: ")    
-    student_obj = Student(username)
-    while True:
-        student_obj.dBManagement.db_students_init()
-        user_input = int(input("Choose option(0 - add / 1 - add_temp / 2 - check / 3 - DB / 4 - reset / 5 - close): "))
-        if user_input == 0:
-            if not student_obj.dBManagement.db_check_student(username):
-                student_obj.photoManagement.take_picture(student_obj.name, Student.images_folder())
-                student_obj.dBManagement.db_add_student(username)
-        elif user_input == 1:
-            student_obj.photoManagement.take_picture(student_obj.name, Student.temporary_folder())
-        elif user_input == 2:
-            if student_obj.dBManagement.db_check_student(username):
-                if student_obj.recognition.check(f"{Student.images_folder()}/{username}.jpg", f"{Student.temporary_folder()}/{username}.jpg"):
-                    print("Fetele se potrivesc")
-                    student_obj.dBManagement.db_record_attendance(username)
-                else:
-                    print("Fetele nu se potrivesc")
-            else:
-                print("Studentul nu este inregistrat")
-        elif user_input == 3:
-            print("DB options")
-            db_choice = int(input("Choose option(0 - get_students / 1 - get_attendance_report / 2 - update_name): "))
-            if db_choice == 0:
-                print(student_obj.dBManagement.db_get_students())
-            elif db_choice == 1:  
-                student_obj.generate_attendance_report()
-            elif db_choice == 2:
-                old_name = input("Enter the old name: ")
-                new_name = input("Enter the new name: ")
-                student_obj.dBManagement.db_update_student(old_name, new_name)
-        elif user_input == 4:
-            Student.reset()
-            break
-        elif user_input == 5:
-            os.system(f"rm -rf {Student.temporary_folder()}/*")
-            print("System shutdown")
-            break
-        else:
-            print("Invalid option")
-            continue
+class App(customtkinter.CTk):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title("Automated Attendance System PreAlpha Release")
+        self.width = self.winfo_screenwidth()
+        self.height = self.winfo_screenheight()
+        self.geometry(f"{self.width}x{self.height}")
+        self.resizable(False, False)
+
+        default_font = tkFont.nametofont("TkDefaultFont")
+        default_font.configure(family="Roboto", size=12)
+        self.option_add("*Font", default_font)
+        self.configure(bg="black")
+
+        self.login_frame = customtkinter.CTkFrame(self, corner_radius=30, height=int(self.height * 0.75), width=int(self.width * 0.5),)
+        self.login_frame.place(relx=0.5, rely=0.45, anchor="center")
+
+        self.login_label = customtkinter.CTkLabel(self.login_frame, text="Admin Panel",
+                                                  font=customtkinter.CTkFont(family="Roboto", size=50, weight="normal", slant="italic"),)
+        self.login_label.grid(row=0, column=0, padx=200, pady=100)
+        self.username_entry = customtkinter.CTkEntry(self.login_frame, width=int(self.width * 0.25), height=int(self.height * 0.05), placeholder_text="Username",
+                                                     font=customtkinter.CTkFont(family="Roboto", size=25, weight="normal", slant="italic"),justify="center")
+        self.username_entry.grid(row=1, column=0, padx=200, pady=20)
+        self.password_entry = customtkinter.CTkEntry(self.login_frame, width=int(self.width * 0.25), height=int(self.height * 0.05), show="*", placeholder_text="Password",
+                                                     font=customtkinter.CTkFont(family="Roboto", size=25, weight="normal", slant="italic"),justify="center")
+        self.password_entry.grid(row=2, column=0, padx=200, pady=20)
+        self.login_button = customtkinter.CTkButton(self.login_frame, text="Login", command=self.login_event, width=int(self.width * 0.25), height=int(self.height * 0.05),
+                                                    font=customtkinter.CTkFont(family="Roboto", size=25, weight="normal", slant="italic"),)
+        self.login_button.grid(row=3, column=0, padx=200, pady=20)
+        self.remember_me_checkbox = customtkinter.CTkCheckBox(self.login_frame, text="Remember Me",
+                                                      font=customtkinter.CTkFont(family="Roboto", size=20, weight="normal", slant="italic"))
+        self.remember_me_checkbox.grid(row=4, column=0, padx=200, pady=(20, 100))
+    
+        self.login_frame.configure(border_color=self.login_button.cget("fg_color"), border_width=2)
+        self.login_frame.grid_rowconfigure(0, weight=1, minsize=50)
+        self.login_frame.grid_rowconfigure(1, weight=1, minsize=50)
+        self.login_frame.grid_rowconfigure(2, weight=1, minsize=50)
+        self.login_frame.grid_rowconfigure(3, weight=1, minsize=50)
+        self.login_frame.grid_rowconfigure(4, weight=1, minsize=50)
+        self.login_frame.grid_columnconfigure(0, weight=1)
+
+
+    def login_event(self):
+        pass
 
 if __name__ == '__main__':
-    main()
-
-    
+    app = App()
+    app.mainloop()
